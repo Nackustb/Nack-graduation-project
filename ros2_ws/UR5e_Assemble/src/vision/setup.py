8ed76b0ddba6ccb36f6103e6ab2d33b4e1d4081c
Nackustb <2249314748@qq.com>
2025-04-17 22:32:20 +0800

    精简ur_ros

--- ros2_ws/UR5e_Assemble/src/vision/setup.py
+++ ros2_ws/UR5e_Assemble/src/vision/setup.py
@@ -9,7 +9,6 @@ submodule_utils_name = 'vision/yolov5/utils'
 setup(
     name=package_name,
     version='0.0.0',
-    # packages=find_packages(exclude=['test']),
     packages=[package_name, submodule_model_name, submodule_utils_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -25,12 +24,9 @@ setup(
     maintainer_email='zitongbai@outlook.com',
     description='TODO: Package description',
     license='TODO: License declaration',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'obj_detect = vision.obj_detect:main',
-            'test_sync = vision.test_sync:main',
-            'test_rs = vision.test_rs:main',
             'obj_seg = vision.obj_seg:main',
             'record = vision.record:main',
             'det_tf = vision.det_tf:main',
